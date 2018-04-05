3b3e40ab308899ee61318199f8531d5323dffbfd
Alyson Paige <alysonstiffel@gmail.com>
2018-04-05 09:53:29 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+function countdownCalc() {
+  // set date to count down to
+  var countdownDate = new Date("June 14, 2018 11:00:00").getTime();
+  // todays date and time
+  var now = new Date().getTime();
+  // find time between now and the coutdown date
+  var time = countdownDate - now;
+
+  if (time >= 0) {
+    // time calculations for days, hours, minutes and seconds
+    var seconds = Math.floor((time / 1000) % 60);
+    var minutes = Math.floor((time / 1000 / 60) % 60);
+    var hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(time / (1000 * 60 * 60 * 24));
+    return {
+      'total': time,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  } else {
+      return {
+      'total': 0,
+      'days': 0,
+      'hours': 0,
+      'minutes': 0,
+      'seconds': 0
+    };
+  }
+};
+
+// initialize clock
+function initializeClock(id, finishTime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+  // display result
+  function displayClock() {
+    var time = countdownCalc(finishTime);
+  
+    daysSpan.innerHTML = time.days;
+    hoursSpan.innerHTML = ('0' + time.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + time.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + time.seconds).slice(-2);
+  
+    // When countdown is finished
+    if (time.total <= 0) {
+      clearInterval(timeinterval);
+      document.getElementById('kickoff').innerHTML = 'Kick off!';
+    }
+  }
+  displayClock();
+  var timeinterval = setInterval(displayClock, 1000);
+};
+
+var deadline = new Date("June 14, 2018 11:00:00").getTime();
+initializeClock('clockdiv', deadline);
